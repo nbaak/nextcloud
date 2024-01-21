69250e01b20f689db677f36b7641cfb9b898911b
k3nny <k3nny@nordgaming.de>
2024-01-21 12:34:27 +0100

    last cron logging

--- cron.sh
+++ cron.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+THIS_DIR=$(dirname $(readlink -f $0))
+
+pushd ${THIS_DIR}
+
 # docker-compose exec -u www-data nextcloud php cron.php > last_cronjob
 docker exec -u www-data nextcloud_app php cron.php > last_cronjob
+
+popd
