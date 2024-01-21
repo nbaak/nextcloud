3f1f5759c701be5a370a6396e748afeae9583b9c
k3nny <k3nny@nordgaming.de>
2024-01-21 12:26:54 +0100

    somehow we need to use docker instead of docker-compose for cron

--- cron.sh
+++ cron.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-THIS_DIR=$(dirname $(readlink -f $0))
-
-pushd ${THIS_DIR}
-
-docker-compose exec -u www-data nextcloud php -f cron.php > last_cronjob
-
-popd
+# docker-compose exec -u www-data nextcloud php cron.php > last_cronjob
+docker exec -u www-data nextcloud_app php cron.php > last_cronjob
