1b974a7756683b4bba2fce0d0df53efe2df38400
nbaak <niklas.baak@outlook.com>
2024-06-17 15:18:24 +0200

    install replaces server name(s) and sets redirects now

--- nginx/install.sh
+++ nginx/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SERVER_NAMES=$1
+
 cp nextcloud.conf /etc/nginx/sites-available
 
 if [ -f *.crt ] && [ -f *.csr ]; then
@@ -9,6 +11,10 @@ cp *.crt /etc/nginx/ssl/
 cp *.csr /etc/nginx/ssl/
 
 sed -i 's/#SSLCert/#SSLCERT\n      listen 443 ssl;\n      ssl_certificate \/etc\/nginx\/ssl\/nextcloud.crt;\n      ssl_certificate_key \/etc\/nginx\/ssl\/nextcloud.csr;/' /etc/nginx/sites-available/nextcloud.conf
+
+sed -i 's/#SSLRedirect/server {\n    server_name #SERVER_NAMES;\n    listen 80;\n    return 301 https:\/\/$host$request_uri;\n}/' /etc/nginx/sites-available/nextcloud.conf
+
+sed -i "s/#SERVER_NAMES/$SERVER_NAMES/g" /etc/nginx/sites-available/nextcloud.conf
 
 else
     echo "no keys found"
