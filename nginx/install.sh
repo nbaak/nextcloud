d8f6645c0691cce83ee200885110398cb5dcae15
nbaak <niklas.baak@outlook.com>
2024-06-17 15:27:44 +0200

    moved sslcert to the right block

--- nginx/install.sh
+++ nginx/install.sh
@@ -10,7 +10,7 @@ mkdir -p /etc/nginx/ssl
 cp *.crt /etc/nginx/ssl/
 cp *.csr /etc/nginx/ssl/
 
-sed -i 's/#SSLCert/#SSLCERT\n      listen 443 ssl;\n      ssl_certificate \/etc\/nginx\/ssl\/nextcloud.crt;\n      ssl_certificate_key \/etc\/nginx\/ssl\/nextcloud.csr;/' /etc/nginx/sites-available/nextcloud.conf
+sed -i 's/#SSLCert/#SSLCERT\n  listen 443 ssl;\n  ssl_certificate \/etc\/nginx\/ssl\/nextcloud.crt;\n  ssl_certificate_key \/etc\/nginx\/ssl\/nextcloud.csr;/' /etc/nginx/sites-available/nextcloud.conf
 
 sed -i 's/#SSLRedirect/server {\n    server_name #SERVER_NAMES;\n    listen 80;\n    return 301 https:\/\/$host$request_uri;\n}/' /etc/nginx/sites-available/nextcloud.conf
 
